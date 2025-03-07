a09d9353e0d8ec362fd9e6f0a529bd8b01ed2929
Vandans304 <vandansonicareers@gmail.com>
2025-03-07 11:09:02 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,70 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Auth from "./pages/auth";
-import Chat from "./pages/chat";
-import Profile from "./pages/profile";
-import { useAppStore } from './store';
-import { apiClient } from './lib/api-client';
-import { GET_USER_INFO } from './utils/constants.js';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const PrivateRoute = ({ children }) => {
-    const { userInfo } = useAppStore();
-    return userInfo ? children : <Navigate to="/auth" />;
-};
+function App() {
+  const [count, setCount] = useState(0)
 
-const AuthRoute = ({ children }) => {
-    const { userInfo } = useAppStore();
-    return userInfo ? <Navigate to="/chat" /> : children;
-};
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p className=''>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-const App = () => {
-    const { userInfo, setUserInfo } = useAppStore();
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const getUserData = async () => {
-            try {
-                const response = await apiClient.get(GET_USER_INFO, { withCredentials: true });
-                if(response.status===200 && response.data.id)
-                {
-                    setUserInfo(response.data)
-                }
-                else
-                {
-                    setUserInfo(undefined)
-                }
-                console.log("User Data:", response.data);
-                
-            } catch (error) {
-                setUserInfo(undefined)
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (!userInfo) {
-            getUserData();
-        } else {
-            setLoading(false);
-        }
-    }, [userInfo, setUserInfo]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/auth" element={<AuthRoute><Auth /></AuthRoute>} />
-                    <Route path="/chat" element={<PrivateRoute><Chat /></PrivateRoute>} />
-                    <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-                    <Route path="*" element={<Navigate to="/auth" />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
-};
-
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import {Toaster} from "./components/ui/sonner"
-import { SocketProvider } from './context/SocketContext';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <SocketProvider>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-    <Toaster closeButton />
-  </SocketProvider>
-);
+  </StrictMode>,
+)
